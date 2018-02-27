223c6c4f8106d4dcff13b7530c276903b47b3dd1
Frol8612 <31982517+Frol8612@users.noreply.github.com>
2018-02-27 13:42:19 +0300

    additional_5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let arr  = str.split(''),
+  sum = 0,
+  st = [];
+  console.log(arr);
+  
+for (let i in arr) {
+  if (arr[i] == '(' || arr[i] == '[' || arr[i] == '{' || arr[i] == '1' || arr[i] == '3' || arr[i] == '5') {
+      st.push(arr[i]);
+      
+  } else if (arr[i] == ')') {
+     if (st.pop() != '(') {
+         return false;
+     }
+  } else if (arr[i] == ']') {
+      if (st.pop() != '[') {
+          return false;
+      }
+  } else if (arr[i] == '}') {
+      if (st.pop() != '{') {
+          return false;
+      }
+  } else if (arr[i] == '2') {
+      if (st.pop() != '1') {
+          return false;
+      }
+  } else if (arr[i] == '4') {
+      if (st.pop() != '3') {
+          return false;
+      }
+  } else if (arr[i] == '6') {
+      if (st.pop() != '5') {
+          return false;
+      }
+  }
+
+}
+var reg = /||/g;
+  if (arr == reg) {
+    return true;
+  }
+
+return !st.length;
 }
