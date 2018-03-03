a8c5440f646d0eff408a7aceeb84954700e04c0b
Frol8612 <31982517+Frol8612@users.noreply.github.com>
2018-03-03 10:41:10 +0300

    additional_5

--- src/index.js
+++ src/index.js
@@ -3,25 +3,50 @@ module.exports = function check(str, bracketsConfig) {
   let arr  = str.split(''),
   sum = 0,
   st = [],
-  prom = [];
+  prom = [],
+  prom7 = [],
+  prom8 = [];
   
   for(let j = 0; j < arr.length; j++) {
-    if (arr[j] == '|' || arr[j] == '7' || arr[j] == '8') {
+    if (arr[j] == '|') {
         prom.push(arr[j]);
         for(let p = 0; p < prom.length; p++) {
             if(p % 2 != 0) {
-                prom[p] = ')';
+                prom[p] = '>';
                 arr[j] = prom[p];
             } else {
-                prom[p] = '(';
+                prom[p] = '<';
                 arr[j] = prom[p];
             }
         }
- }
-    
-} 
+    } else if (arr[j] == '7') {
+        prom7.push(arr[j]);
+        for(let p = 0; p < prom7.length; p++) {
+            if(p % 2 != 0) {
+                prom7[p] = '?';
+                arr[j] = prom7[p];
+            } else {
+                prom7[p] = '!';
+                arr[j] = prom7[p];
+            }
+        }
+      } if (arr[j] == '8') {
+        prom8.push(arr[j]);
+        for(let p = 0; p < prom8.length; p++) {
+            if(p % 2 != 0) {
+                prom8[p] = '.';
+                arr[j] = prom8[p];
+            } else {
+                prom8[p] = ',';
+                arr[j] = prom8[p];
+            }
+        }
+    }
+
+  }
+
 for (let i in arr) {
-  if (arr[i] == '(' || arr[i] == '[' || arr[i] == '{' || arr[i] == '1' || arr[i] == '3' || arr[i] == '5') {
+  if (arr[i] == '(' || arr[i] == '[' || arr[i] == '{' || arr[i] == '1' || arr[i] == '3' || arr[i] == '5' || arr[i] == '<' || arr[i] == '!' || arr[i] == ',') {
       st.push(arr[i]);
       
   } else if (arr[i] == ')') {
@@ -48,7 +73,19 @@ for (let i in arr) {
       if (st.pop() != '5') {
           return false;
       }
-  }
+  } else if (arr[i] == '>') {
+      if (st.pop() != '<') {
+          return false;
+      }
+  } else if (arr[i] == '?') {
+    if (st.pop() != '!') {
+        return false;
+    }
+} else if (arr[i] == '.') {
+    if (st.pop() != ',') {
+        return false;
+    }
+}
 }
 
 return !st.length;
