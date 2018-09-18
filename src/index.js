d86a4a4b1d39ff26a4e2b7efbf707a5d25f703bb
Frol8612 <infern00730@gmail.com>
2018-09-18 10:33:51 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,92 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-  let arr  = str.split(''),
-  sum = 0,
-  st = [],
-  prom = [],
-  prom7 = [],
-  prom8 = [];
-  
-  for(let j = 0; j < arr.length; j++) {
-    if (arr[j] == '|') {
-        prom.push(arr[j]);
-        for(let p = 0; p < prom.length; p++) {
-            if(p % 2 != 0) {
-                prom[p] = '>';
-                arr[j] = prom[p];
-            } else {
-                prom[p] = '<';
-                arr[j] = prom[p];
-            }
+    // your solution
+
+    class Search {
+        constructor(str, config) {
+            this.arr = [];
+            this.str = str.split('');
+            this.config = config;
         }
-    } else if (arr[j] == '7') {
-        prom7.push(arr[j]);
-        for(let p = 0; p < prom7.length; p++) {
-            if(p % 2 != 0) {
-                prom7[p] = '?';
-                arr[j] = prom7[p];
-            } else {
-                prom7[p] = '!';
-                arr[j] = prom7[p];
+
+
+        matchSearch() {
+            const {str, arr, config} = this;
+
+            if(str.length % 2 !== 0) {
+                return false;
             }
-        }
-      } if (arr[j] == '8') {
-        prom8.push(arr[j]);
-        for(let p = 0; p < prom8.length; p++) {
-            if(p % 2 != 0) {
-                prom8[p] = '.';
-                arr[j] = prom8[p];
-            } else {
-                prom8[p] = ',';
-                arr[j] = prom8[p];
-            }
+            
+            str.forEach(el => {
+                config.forEach(conf => {
+                    if (conf[1] === el && conf[0] === arr[arr.length - 1])
+                        arr.pop();
+                    else if (conf[0] === el) {
+                        arr.push(el);
+                    }
+                });
+            });
+
+            return !arr.length;
         }
     }
+    
 
-  }
+    let search = new Search(str, bracketsConfig);
 
-for (let i in arr) {
-  if (arr[i] == '(' || arr[i] == '[' || arr[i] == '{' || arr[i] == '1' || arr[i] == '3' || arr[i] == '5' || arr[i] == '<' || arr[i] == '!' || arr[i] == ',') {
-      st.push(arr[i]);
-      
-  } else if (arr[i] == ')') {
-     if (st.pop() != '(') {
-         return false;
-     }
-  } else if (arr[i] == ']') {
-      if (st.pop() != '[') {
-          return false;
-      }
-  } else if (arr[i] == '}') {
-      if (st.pop() != '{') {
-          return false;
-      }
-  } else if (arr[i] == '2') {
-      if (st.pop() != '1') {
-          return false;
-      }
-  } else if (arr[i] == '4') {
-      if (st.pop() != '3') {
-          return false;
-      }
-  } else if (arr[i] == '6') {
-      if (st.pop() != '5') {
-          return false;
-      }
-  } else if (arr[i] == '>') {
-      if (st.pop() != '<') {
-          return false;
-      }
-  } else if (arr[i] == '?') {
-    if (st.pop() != '!') {
-        return false;
-    }
-} else if (arr[i] == '.') {
-    if (st.pop() != ',') {
-        return false;
-    }
-}
-}
-
-return !st.length;
-}
+    return search.matchSearch();
+};
